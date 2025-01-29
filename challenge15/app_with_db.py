3fe98c0b1d8a705dc3a7881da8e58af04c18972a
omriwejden <omriouejden@gmail.com>
2025-01-29 18:45:06 +0100

    Initial Kubernetes project with Helm charts

---
+++ challenge15/app_with_db.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+MONGO_HOST = os.environ.get("MONGO_HOST", "localhost")
+MONGO_PORT = int(os.environ.get("MONGO_PORT", 27017))
+MONGO_DB = os.environ.get("MONGO_DB", "mydatabase")
+version = "2.0"
+@app.route("/")
+def index():
+    client_ip = request.remote_addr
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Connexion à MongoDB
+    client = MongoClient(host=MONGO_HOST, port=MONGO_PORT)
+    db = client[MONGO_DB]
+    collection = db["requests"]
+    collection.insert_one({"ip": client_ip, "time": current_time})
+
+    # Récupérer les 10 derniers enregistrements
+    records = collection.find().sort("time", -1).limit(10)
+    history = "\n".join([f"{r['ip']} - {r['time']}" for r in records])
+
+    # Récupérer le nom du pod depuis la variable d'environnement HOSTNAME
+    pod_name = os.environ.get("HOSTNAME", "Unknown Pod")
+
+    return f"""
+    <h1>Flask App with MongoDB</h1>
+    <p>Client IP: {client_ip}</p>
+    <p><strong>Pod Name:</strong> {pod_name}</p>
+    <p>Current Time: {current_time}</p>
+    <p><strong>Version:</strong> {version}</p> <!-- Version affichée -->
+    <p><strong>Last 10 Records:</strong></p>
+    <pre>{history}</pre>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
---
+++ challenge15/app_without_db.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # Static information
+    my_name = "Wejden"
+    project_name = "My Web Project"
+    version = "2.0"
+    
+    # Dynamic information
+    hostname = socket.gethostname()  
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+    
+    # HTML content
+    html_content = f"""
+    <html>
+      <head><title>{project_name} - {version}</title></head>
+      <body>
+        <h1>Welcome to {project_name}!</h1>
+        <p><strong>Name:</strong> {my_name}</p>
+        <p><strong>Project Name:</strong> {project_name}</p>
+        <p><strong>Hostname:</strong> {hostname}</p>
+        <p><strong>Version:</strong> {version}</p>
+        <p><strong>Current Date:</strong> {current_date}</p>
+        <hr/>
+      </body>
+    </html>
+    """
+    return html_content
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
